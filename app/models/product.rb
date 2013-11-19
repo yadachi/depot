e909a9eaf5e17f338012e01d1abb3dd06b93963a
Yosuke Adachi <front.lightstone@gmail.com>
2013-11-19 23:33:39 +0000

    add prodocts_controller_test.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ActiveRecord::Base
 	validates :title, :description, :image_url, Presence: true
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
+	validates :title, uniqueness: true
+	validates :image_url, allow_blank: true, format: {
+		with:  %r{\A(gif|jpg|png)\z}i,
+		message: 'must be gif, jpg or png extention'
+	}
 end
