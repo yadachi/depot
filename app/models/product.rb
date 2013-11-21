fe46a5b1394cee1ce8ae8a7fb3bfde18fe633de8
Yosuke Adachi <front.lightstone@gmail.com>
2013-11-21 13:38:05 +0000

    modified validate image_url

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ActiveRecord::Base
 	validates :price, numericality: {greater_than_or_equal_to: 0.01}
 	validates :title, uniqueness: true
 	validates :image_url, allow_blank: true, format: {
-		with:  %r{\A(gif|jpg|png)\z}i,
+		with:  /\.(gif|jpg|png)/i,
 		message: 'must be gif, jpg or png extention'
 	}
 end
