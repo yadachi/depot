bfa7fce92b59268e0c81e14d21d03cf1725eeff9
Yosuke Adachi <front.lightstone@gmail.com>
2013-12-18 22:23:36 +0000

    add store index

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -4,10 +4,10 @@ class ProductsControllerTest < ActionController::TestCase
   setup do
     @product = products(:one)
     @update = {
-      title:        'Lorem Ipsum'
-      description:  'Wibbles are fun!'
-      image_url:    'lorem.jpg'
-      price:        19.95 
+      title:        'Lorem Ipsum',
+      description:  'Wibbles are fun!',
+      image_url:    'lorem.jpg',
+      price:        19.95, 
      }
   end
 
