891ed7d2b580cafacb1e69f939b006d758261fd8
Yosuke Adachi <front.lightstone@gmail.com>
2014-01-14 23:52:55 +0000

    add skip_before_filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :authorize
 
   private
 
@@ -12,4 +13,10 @@ class ApplicationController < ActionController::Base
   	  session[:cart_id] = cart.id
   	  cart
   	end
+
+    def authorize
+      unless User.find_by_id(session[:user_id])
+        redirect_to login_url, notice: "Please login"
+      end
+    end
 end
